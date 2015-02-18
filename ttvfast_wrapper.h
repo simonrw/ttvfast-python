ce28508212d6874248265f2e32b78141f1ddcd11
Simon Walker <s.r.walker101@googlemail.com>
2015-02-18 13:59:45 +0000

    Remove unused wrapper header file

--- ttvfast_wrapper.h
+++
@@ -1,4 +0,0 @@
-#include "myintegrator.h"
-#include "transit.h"
-
-void TTVFast(double *params,double dt, double Time, double total,int n_plan,CalcTransit *transit,CalcRV *RV_struct, int nRV, int n_events, int input_flag);
