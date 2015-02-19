2f1a16d841728ca4ee7766b81bc2044cc5fa4031
Simon Walker <s.r.walker101@googlemail.com>
2015-02-19 14:51:37 +0000

    Move ttvfast_wrapper to src dir

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, Extension
 import numpy 
 
 ttvfast = Extension("ttvfast", 
-            sources=["ttvfast_wrapper.c", "./TTVFast/c_version/TTVFast.c"],
+            sources=["src/ttvfast_wrapper.c", "./TTVFast/c_version/TTVFast.c"],
             include_dirs=['TTVFast/c_version', numpy.get_include()],
             # Debug mode
             # define_macros=[('DEBUG', True)],
