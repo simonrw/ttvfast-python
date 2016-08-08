3b5b974e455181335557f1d5aaab438ca545b31a
Simon Walker <s.r.walker101@googlemail.com>
2016-08-08 20:26:31 +0100

    Prevent infinite loop
    Bug reported by Andrew Mayo, during his MCMC process the code would
    hang. This is extra to the process dying error as reported by Lauren
    Weiss.
    
    One of the do-while loops did not have an iteration-based exit
    condition, only a convergence criterion and with bad input was causing
    an infinite loop.
    
    This change adds a check with `MAX_ITER` and will exit the loop if the
    maximum number of iterations is met.
    
    I have a concern that the current value of 35 may not be enough for this
    particular loop. It is possible that this value needs raising.
    
    The code changes are in the subproject so check there for the actual
    diff.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as infile:
 
 setup(
     name='ttvfast',
-    version='0.1.0',
+    version='0.1.1',
     description='Python wrapper to ttvfast',
     url='https://github.com/mindriot101/ttvfast-python',
     long_description=long_description,
