eea8599519d710919382bb9d726225e02e88eaf8
Simon Walker <s.r.walker101@googlemail.com>
2016-10-10 15:22:24 +0100

    Allow additional input coordinate types (#10)
    * Update required pytest-cov package
    
    * Update `ttvfast` function to accept custom input_flag entry
    
    Closes #9
    
    * Update ttvfast to include rebased content

--- testing/test_python_api.py
+++ testing/test_python_api.py
@@ -1,3 +1,4 @@
+import pytest
 import numpy as np
 
 import ttvfast
@@ -29,3 +30,12 @@ def test_module_docstring_is_present():
 
 def test_ttvfast_docstring_is_present():
     assert 'https://github.com/kdeck/TTVFast' in ttvfast.ttvfast.__doc__
+
+
+@pytest.mark.parametrize('input_flag', [-1, -100, 3, 4, 5, 100])
+def test_bad_input_flags(stellar_mass, planets, python_args, input_flag):
+    Time, dt, Total = python_args
+    with pytest.raises(ValueError) as exc_info:
+        ttvfast.ttvfast(planets, stellar_mass, Time, dt, Total, input_flag=input_flag)
+
+    assert 'invalid `input_flag`' in str(exc_info).lower()
--- ttvfast/__init__.py
+++ ttvfast/__init__.py
@@ -9,7 +9,7 @@ from . import models
 __all__ = ['ttvfast']
 
 
-def ttvfast(planets, stellar_mass, time, dt, total, rv_times=None):
+def ttvfast(planets, stellar_mass, time, dt, total, rv_times=None, input_flag=0):
     '''
     Run the TTV fast function. See https://github.com/kdeck/TTVFast.
     Program arguments:
@@ -19,10 +19,18 @@ def ttvfast(planets, stellar_mass, time, dt, total, rv_times=None):
     * dt: time step for the integration (days)
     * total: end point for integration (days)
     * rv_times: rv measurement times
+    * input_flag: input coordinate system (0, 1 or 2)
+
+    The `input_flag` argument corresponds to:
+        0 = Jacobi
+        1 = astrocentric elements
+        2 = astrocentric cartesian
     '''
+    if input_flag not in [0, 1, 2]:
+        raise ValueError('Invalid `input_flag` value. Must be 0, 1 or 2')
+
     params = models.planets_to_params(stellar_mass, planets)
     n_plan = len(planets)
-    input_flag = 0
 
     len_rv = len(rv_times) if rv_times is not None else 0
     positions, rv = _ttvfast_fn(
